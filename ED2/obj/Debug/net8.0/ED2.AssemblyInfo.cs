dae8302da08dcd7451ee43fa7ccbbb56adacfb59
Uriel Abraham <abraham2160@hotmail.com>
2025-07-11 12:03:31 -0600

    Archivos compilación

--- ED2/obj/Debug/net8.0/ED2.AssemblyInfo.cs
+++ ED2/obj/Debug/net8.0/ED2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ED2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa768dc1e0caef3f2ae58d7022d8cc701dda6d16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77aa9012a71c511983819c07bcbf41f8665d705d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ED2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ED2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
