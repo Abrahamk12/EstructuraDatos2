773755215426cc1c51055ee1611b5fcf2f0f51cc
Uriel Abraham <abraham2160@hotmail.com>
2025-07-26 09:43:24 -0700

    QuickSort

--- ED2/Program.cs
+++ ED2/Program.cs
@@ -1,74 +1,51 @@
 ﻿namespace ED2;
 using System;
 using System.Collections;
-class HashTable
-{
-    private int size;
-    private List<KeyValuePair<int, string>>[] table;
-    public HashTable(int size)
-    {
-        this.size = size;
-        table = new List<KeyValuePair<int, string>>[size];
-        for (int i = 0; i < size; i++)
-        {
-            table[i] = new List<KeyValuePair<int, string>>();
-        }
 
-    }
-    private int Hashfunction(int key)
-    {
-        return key % size;
-    }
-
-    public void Insert(int key, string _value)
-    {
-        int index = Hashfunction(key);
-        table[index].Add(new KeyValuePair<int, string>(key, _value));
-    }
-    public string Search(int key)
-    {
-        int index = Hashfunction(key);
-        foreach (var pair in table[index])
-        {
-            if (pair.Key == key)
-            {
-                return pair.Value;
-            }
-        }
-        return null;
-    }
-    public void Display()
-    {
-        for (int i = 0; i < size; i++)
-        {
-            Console.Write($"Index {i}: ");
-            foreach (var pair in table[i])
-            {
-                Console.Write($"[{pair.Key}, {pair.Value}] ");
-            }
-            Console.WriteLine();
-        }
-    }
-
-}
 class Program
 {
-    static void Main(string[] args)
+    static void QuickSort(int[] array, int left, int right)
     {
-        HashTable ht = new HashTable(10);
-        ht.Insert(0, "patata");
-        ht.Insert(1, "One");
-        ht.Insert(2, "Two");
-        ht.Insert(9, "Twelve");
+        if (left < right)
+        {
+            int pivotIndex = Partition(array, left, right);
+            QuickSort(array, left, pivotIndex - 1);
+            QuickSort(array, pivotIndex + 1, right);
+        }
+    }
 
-        ht.Display();
+    static int Partition(int[] array, int left, int right)
+    {
+        int pivot = array[right];
+        int i = left - 1;
+
+        for (int j = left; j < right; j++)
+        {
+            if (array[j] <= pivot)
+            {
+                i++;
+                Swap(array, i, j);
+            }
+        }
+
+        Swap(array, i + 1, right);
+        return i + 1;
+    }
+
+    static void Swap(int[] array, int a, int b)
+    {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+
+    static void Main()
+    {
+        int[] data = { 8, 3, 1, 7, 0, 10, 2 };
+        Console.WriteLine("Original array: " + string.Join(", ", data));
+
+        QuickSort(data, 0, data.Length - 1);
+
+        Console.WriteLine("Sorted array: " + string.Join(", ", data));
     }
 }
-
-/*
-
-Desde la terminal, en la carpeta del proyecto:
-dotnet build - copilar
-dotnet run - ejecutar
-
-*/
